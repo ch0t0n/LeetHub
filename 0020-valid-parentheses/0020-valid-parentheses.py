c7797da4a00eec7a4b37fdb888e529ac63d0cabe
ch0t0n <89215763+ch0t0n@users.noreply.github.com>
2022-10-11 16:31:55 -0500

    Time: 33 ms (47.07%), Space: 13.6 MB (30.14%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,7 +4,6 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        n = len(s)
         st = []
         for c in s:
             if c == '(' or c == '{' or c=='[':
