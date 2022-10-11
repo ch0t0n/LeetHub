353bdc4086a6677dcb186aeef1c8a98ef6f778f8
ch0t0n <89215763+ch0t0n@users.noreply.github.com>
2022-10-11 08:51:30 -0500

    Time: 305 ms, Memory: 15.9 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,8 +1,36 @@
 class Solution(object):
     def sortedSquares(self, nums):
-        for i, v in enumerate(nums):
-            nums[i] = v**2
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        negs = []
+        n = len(nums)
+        pos = []
+        for i in range(n):
+            if nums[i] < 0:
+                negs.append(nums[i])
+            else:
+                pos = nums[i:]
+                break
         
-        nums = sorted(nums)
-        
-        return nums
+        i, j = len(negs)-1, 0
+        squared = []
+        while i>=0 and j<len(pos):
+            a, b = negs[i]**2, pos[j]**2
+            if a < b:
+                squared.append(a)
+                i -= 1
+            else:
+                squared.append(b)
+                j += 1
+        while i>=0:
+            a = negs[i]**2
+            squared.append(a)
+            i -= 1
+        while j<len(pos):
+            b = pos[j]**2
+            squared.append(b)
+            j += 1
+        return squared
+                
